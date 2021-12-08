00b259b030d8fca49a0745ed74315a4e39cc1060
Ilya Shpilkov <ilya.shpilkov@mail.ru>
2021-12-08 22:39:26 +0300

    Implement console arguments handle

---
+++ life/life.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#define INPUT_ARG           "--input"
+#define OUTPUT_ARG          "--output"
+#define MAX_ITER_ARG        "--max_iter"
+#define DUMP_FREQ_ARG       "--dump_freq"
+
+int main(int argc, char* argv[]) {
+    FILE*       input       = NULL;
+    char*       output_dir  = NULL;
+    uint32_t    max_iter    = 0;    // 0 represent inf value
+    uint32_t    dump_freq   = 1;
+
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], INPUT_ARG) == 0) {
+            if (i + 1 == argc) {
+                fprintf(stderr, "Input file name wasn't provided\n");
+                goto CLEANUP;
+            }
+
+            input = fopen(argv[i + 1], "rwb+");
+
+            if (!input) {
+                fprintf(stderr, "Couldn't open input file: %s\n", argv[i + 1]);
+                goto CLEANUP;
+            }
+        }
+        
+        if (strcmp(argv[i], OUTPUT_ARG) == 0) {
+            if (i + 1 == argc) {
+                fprintf(stderr, "Argument output wasn't provided\n");
+                goto CLEANUP;
+            }
+
+            char* output_dir = argv[i + 1];
+        }
+
+        if (strcmp(argv[i], MAX_ITER_ARG) == 0) {
+            if (i + 1 == argc) {
+                fprintf(stderr, "Argument max_iter wasn't provided\n");
+                goto CLEANUP;
+            }
+
+            if ((max_iter = atoi(argv[i + 1])) == 0) {
+                fprintf(stderr, "Invalid max_iter argument: %s %s\n", argv[i], argv[i + 1]);
+                goto CLEANUP;
+            }
+        }
+
+        if (strcmp(argv[i], DUMP_FREQ_ARG) == 0) {
+            if (i + 1 == argc) {
+                fprintf(stderr, "Argument dump_freq wasn't provded\n");
+                goto CLEANUP;
+            }
+
+            if ((dump_freq = atoi(argv[i + 1])) == 0) {
+                fprintf(stderr, "Invalid dump_freq argument: %s %s\n", argv[i], argv[i + 1]);
+                goto CLEANUP;
+            }
+        }
+    }
+
+CLEANUP:
+    if (input) {
+        fclose(input);
+    }
+
+    return 0;
+}
