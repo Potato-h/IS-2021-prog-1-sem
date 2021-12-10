81d1a21927e20c1d21ce622094f99940bfdff2a4
Ilya Shpilkov <ilya.shpilkov@mail.ru>
2021-12-10 12:35:41 +0300

    Implement mult_op tests

--- uint1024_t/gtests/tests.cpp
+++ uint1024_t/gtests/tests.cpp
@@ -58,6 +58,25 @@ TEST (uint1024_t, subtract) {
     }
 }
 
+TEST (uint1024_t, multiply) {
+    std::map<std::pair<const char*, const char*>, const char*> tests = {
+        { { "129389328913", "0" }, "0" },
+        { { "713781723881381723", "139183091832817387" }, "99346347223569055729293617498417801" },
+        { { "12738917293871238712", "9130918293081938138" }, "116318012952666753505166366727814798256" },
+        { { "9183091832098120938109381238", "1203102930193123" }, "11048204691429783592024030637855325372826274" },
+        { { "8138917239871293871387198739817398173981273987123", "913891391839183291832" }, "7438086404409900778395230106916925777965772782271025915731990719079336" }
+    };
+
+    for (auto test : tests) {
+        auto input = test.first;
+        uint1024_t x = stoi(input.first);
+        uint1024_t y = stoi(input.second);
+        uint1024_t res = mult_op(x, y);
+        char* obtained = itos(res);
+        EXPECT_EQ(std::string(test.second), std::string(obtained));
+    }
+}
+
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
