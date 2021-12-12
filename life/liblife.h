94e7b16d42404f842243c7747f2fc8b77fbd819a
Ilya Shpilkov <ilya.shpilkov@mail.ru>
2021-12-12 17:56:56 +0300

    doc: add liblife documentation

--- life/liblife.h
+++ life/liblife.h
@@ -4,6 +4,7 @@
 #include "libbmp.h"
 #include <stdint.h>
 
+// Config of life game. All fields are requaried
 struct life_config {
     struct bmp_image*   start;
     char*               out_dir;
@@ -11,18 +12,21 @@ struct life_config {
     uint32_t            dump_freq;
 };
 
-// Can hold all possiable position in hash_set or hash_map
-// If I need in hash_set or hash_map, then hash() is needed
+// State of current game. All fields will be
+// automaticly filled by life_game_init call.
 struct life_game {
     uint32_t            step;
     struct bmp_image*   state;
     struct life_config  config;  
 };
 
+// Initialization of game.
 void life_game_init(struct life_config* config, struct life_game** game);
 
+// One step of game. life_game_init call before first step is required
 int life_game_step(struct life_game* game);
 
+// Clean up of all resources
 void life_free_game(struct life_game** game);
 
 #endif
